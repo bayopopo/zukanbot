3751ab144df4fef4beb09b2febcc1615bafe6f76
bayopopo <57038177+bayopopo@users.noreply.github.com>
2021-05-10 08:19:50 +0900

    Update zukanbot.py

--- zukanbot.py
+++ zukanbot.py
@@ -19,18 +19,19 @@ async def on_ready():
 async def on_message(message):
     global urls
 
-    weights = [1] * 143 + [0.5] * 7 + [0.3] + [0.1]
+    weights = [1] * 143 + [1] * 7 + [0.5] + [0.3]
 
-    if message.author.bot:
+  if message.author.bot:
         return
-    if message.content == "<:monb:641248096495599618>" or message.content =="<:monb:828193362981027880>" or message.content == "<:monb:830720172762333236>":
-        url = random.choices(list(urls.values()), weights=weights)[0]:
-    
+    if message.content == "<:monbo:636725781749301250>" or message.content == "<:monb:830720172762333236>" or message.content =="<:monb:641248096495599618>" or message.content =="<:pokemon:677029538554839096>" or message.content == "<:pokeball:657822336510197760>":
+        url = random.choices(list(urls.values()), weights=weights)[0]
+
         await message.channel.send(url)
 
 
 
         #await message.channel.send(urls["120 ヒトデマン"])
+
 
 
 
