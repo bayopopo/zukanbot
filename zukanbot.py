f043964fd616a67aaf9387dbeabbba97d8aceeda
bayopopo <57038177+bayopopo@users.noreply.github.com>
2021-06-06 15:03:40 +0900

    Update zukanbot.py

--- zukanbot.py
+++ zukanbot.py
@@ -23,7 +23,7 @@ async def on_message(message):
 
     if message.author.bot:
         return
-    if message.content == "<:monb:641248096495599618>" or message.content =="<:monb:828193362981027880>" or message.content == "<:monb:830720172762333236>":
+    if message.content == "<:monb:641248096495599618>" or message.content =="<:monb:828193362981027880>" or message.content == "<:monb:830720172762333236>" or message.content == "<:monb:840951864127651871>":
         url = random.choices(list(urls.values()), weights=weights)[0]
 
         await message.channel.send(url)
