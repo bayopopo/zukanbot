66d743da0a3ca4ae9b77736d3cc08572a30b6e73
bayopopo <57038177+bayopopo@users.noreply.github.com>
2023-11-09 05:44:27 +0900

    !pokemonコマンドに対応。

--- main.py
+++ main.py
@@ -6,27 +6,27 @@ import json
 
 intents = discord.Intents.default()
 intents.message_content = True
-client = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 with open('pokemon.json', encoding='utf-8') as f:
     urls = json.load(f)
 
-@client.event
+@bot.event
 async def on_ready():
-    print('ログインしました')
+    print('Logged in as', bot.user)
 
-@client.event
-async def on_message(message):
+@bot.slash_command(name="pokemon", description="Send a random Pokémon picture")
+async def send_random_pokemon(interaction):
     global urls
 
     weights = [1] * 143 + [0.5] * 7 + [0.3] + [0.1]
 
-    if message.author.bot:
-        return
-    if message.content == "<:monb:1171846145698431099>" or message.content == "<:pokeball:641273768957968404>" or message.content =="<:monb:641248096495599618>" or message.content =="<:pokemon:677029538554839096>" or message.content == "<:pokeball:657822336510197760>":
-        url = random.choice(list(urls.values()), weights=weights)[0]
+    url = random.choice(list(urls.values()), weights=weights)[0]
+    await interaction.response.send_message(url)
 
-        await message.channel.send(url)
+my_secret = os.getenv('DISCORD_TOKEN')
+if my_secret is None:
+    print("DISCORD_TOKEN is not set.")
+else:
+    bot.run(my_secret)
 
-my_secret = 'NjM2NTYwMTA4Nzc5ODY0MDk1.G7w8uh.eBb30Bu5cfL2aa4u-d3WsYCtiP5ZGMC4JAZawc'
-client.run(my_secret)
