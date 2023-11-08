158ebe0f28334db0fa03d5c02a2a199766b39595
bayopopo <57038177+bayopopo@users.noreply.github.com>
2023-11-09 02:02:38 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import discord
+from discord.ext import commands
 import random
 import os
 import json
 
+intents = discord.Intents.default()
+intents.message_content = True
+client = commands.Bot(command_prefix='!', intents=intents)
 
 with open('pokemon.json', encoding='utf-8') as f:
     urls = json.load(f)
 
-print(len(urls))
-client = discord.Client()
-
 @client.event
 async def on_ready():
     print('ログインしました')
-
 
 @client.event
 async def on_message(message):
@@ -23,19 +23,10 @@ async def on_message(message):
 
     if message.author.bot:
         return
-    if message.content == "<:monbo:636725781749301250>" or message.content == "<:pokeball:641273768957968404>" or message.content =="<:monb:641248096495599618>" or message.content =="<:pokemon:677029538554839096>" or message.content == "<:pokeball:657822336510197760>":
-        url = random.choices(list(urls.values()), weights=weights)[0]
+    if message.content == "<:monb:1171846145698431099>" or message.content == "<:pokeball:641273768957968404>" or message.content =="<:monb:641248096495599618>" or message.content =="<:pokemon:677029538554839096>" or message.content == "<:pokeball:657822336510197760>":
+        url = random.choice(list(urls.values()), weights=weights)[0]
 
         await message.channel.send(url)
 
-
-
-        #await message.channel.send(urls["120 ヒトデマン"])
-
-
-
-client.run(os.environ["DISCORD_TOKEN"])
-
-
-my_secret = os.environ['DISCORD_TOKEN']
+my_secret = 'NjM2NTYwMTA4Nzc5ODY0MDk1.G7w8uh.eBb30Bu5cfL2aa4u-d3WsYCtiP5ZGMC4JAZawc'
 client.run(my_secret)
