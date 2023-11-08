8272d44053ab5e8d1868a49d19dc06ef7165c318
bayopopo <57038177+bayopopo@users.noreply.github.com>
2023-11-09 02:43:01 +0900

    discord.py v2に対応

--- main.py
+++ main.py
@@ -6,16 +6,21 @@ import json
 
 intents = discord.Intents.default()
 intents.message_content = True
-client = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-with open('pokemon.json', encoding='utf-8') as f:
+# ファイル 'pokemon.json' のパスを修正
+# Replit.comではファイルへの直接のファイルパス指定はサポートされていないため、ファイルをアップロードして使用します
+# ファイルをアップロードして、ファイル名を 'pokemon.json' に設定してください
+file_path = 'pokemon.json'
+
+with open(file_path, encoding='utf-8') as f:
     urls = json.load(f)
 
-@client.event
+@bot.event
 async def on_ready():
     print('ログインしました')
 
-@client.event
+@bot.event
 async def on_message(message):
     global urls
 
@@ -24,9 +29,10 @@ async def on_message(message):
     if message.author.bot:
         return
     if message.content == "<:monb:1171846145698431099>" or message.content == "<:pokeball:641273768957968404>" or message.content =="<:monb:641248096495599618>" or message.content =="<:pokemon:677029538554839096>" or message.content == "<:pokeball:657822336510197760>":
-        url = random.choice(list(urls.values()), weights=weights)[0]
+        # random.choice() から random.choices() に変更
+        url = random.choices(list(urls.values()), weights=weights, k=1)[0]
 
         await message.channel.send(url)
 
-my_secret = 'YOUR_BOT_TOKEN'
-client.run(my_secret)
+my_secret = os.environ['DISCORD_TOKEN']
+bot.run(my_secret)
